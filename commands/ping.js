b8278dfe39cfb29a2412fb0f2dd093ff92da3e79
Alexandre <alexandre.ricard@epitech.eu>
2024-05-27 11:46:53 +0200

    ADD: basic command

---
+++ commands/ping.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("reply with pong"),
+    async execute(interaction) {
+        await interaction.reply(`pong`);
+    },
+};
